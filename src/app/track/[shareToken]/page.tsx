93d3457aa23869cda6c845b73c9ecac2a8897131
Jeevan-Abishek <jeevanabishek120608@gmail.com>
2026-08-02 21:44:16 +0530

    Delete src directory

--- src/app/track/[shareToken]/page.tsx
+++
@@ -1,7 +0,0 @@
-import { PublicTracker } from "./public-tracker";
-
-export const dynamic = "force-dynamic";
-
-export default function PublicTrackPage({ params }: { params: { shareToken: string } }) {
-  return <PublicTracker shareToken={params.shareToken} />;
-}
